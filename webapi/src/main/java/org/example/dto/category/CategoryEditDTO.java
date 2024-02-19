e43b9d6dbb7252db522fcb41e906b74f67fe73b0
maxik20192006max@gmail.com <maxik20192006max@gmail.com>
2024-02-19 19:25:10 +0200

    WebAPI Create and Get categories

---
+++ webapi/src/main/java/org/example/dto/category/CategoryEditDTO.java
@@ -0,0 +1,13 @@
+package org.example.dto.category;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class CategoryEditDTO {
+    private int id;
+    private String name;
+    private MultipartFile image;
+    private String description;
+    private String dateCreated;
+}
